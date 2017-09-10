5f6ad2ca2a2f49190c268885d57348e8720e95be
Jakub Dorňák <jakub.dornak@misli.cz>
2017-09-10 21:15:43 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM leprikon/leprikon
+
+MAINTAINER Jakub Dorňák <jakub.dornak@misli.com>
+
+# install other dependencies
+COPY requirements.txt /app/requirements.txt
+RUN pip install --no-cache-dir -r /app/requirements.txt
+
+# copy files
+COPY ddmpelhrimov /app/ddmpelhrimov
+
+ENV SITE_MODULE=ddmpelhrimov
+
+# run this command at the end of any dockerfile based on this one
+RUN leprikon collectstatic --no-input
---
+++ ddmpelhrimov/settings.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+"""
+Django settings for ddmpelhrimov project.
+"""
+
+from django.utils.translation import ugettext_lazy as _
+from leprikon.site.settings import *
+
+# Application definition
+INSTALLED_APPS = [
+    'ddmpelhrimov',
+] + INSTALLED_APPS + [
+    'haystack',
+    'aldryn_search',
+]
+
+#ROOT_URLCONF = 'ddmpelhrimov.urls'
+
+#WSGI_APPLICATION = 'ddmpelhrimov.wsgi.application'
+
+CMS_TEMPLATES = [
+    ('default.html', 'Výchozí'),
+]
+
+# templates used to render plugin article
+CMS_ARTICLES_PLUGIN_ARTICLE_TEMPLATES = [
+    ('default', 'Výchozí'),
+]
+
+# templates used to render plugin articles
+CMS_ARTICLES_PLUGIN_ARTICLES_TEMPLATES = [
+    ('default', 'Výchozí'),
+]
+
+CMS_PLACEHOLDER_CONF = {
+    'content': {
+        'name': 'Obsah',
+    },
+}
+
+THUMBNAIL_ALIASES = {
+    '': {
+        'preview': {
+            'size': (240, 10000),
+        },
+        'view': {
+            'size': (760, 570),
+        },
+    },
+}
+
+# search settings
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(BASE_DIR, 'data', 'search'),
+    },
+    'cs': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(BASE_DIR, 'data', 'search'),
+    },
+}
